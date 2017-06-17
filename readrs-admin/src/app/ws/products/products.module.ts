4f9d34e466b612cb85205a7a497233d709f4b687
Виталий Вострокнутов <vostroknutov@hotmail.com>
2017-06-17 12:38:11 +0300

    styles fix #

--- readrs-admin/src/app/ws/products/products.module.ts
+++ readrs-admin/src/app/ws/products/products.module.ts
@@ -2,22 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { ProductsComponent } from './products.component';
 import { ProductsRouterModule } from './products.router.module'; 
-import { SharedModule } from '../../shared/shared.module';
+//import { SharedModule } from '../../shared/shared.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-
-//import { SignInComponent } from './signin/signin.component';
-//import { LoginComponent } from './login/login.component';
 
 @NgModule({
 	imports: [
-		SharedModule,
+		//SharedModule,
 		Ng2SmartTableModule,
 		ProductsRouterModule
 	],
 	declarations: [
-		ProductsComponent,
-		//SignInComponent,
-		//LoginComponent
+		ProductsComponent
 	]
 })
 export class ProductsModule {}
